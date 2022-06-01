7dd42b0fa1293b2d6ae38ae60217da41738e7fdd
kari-1708 <kmstudies143@gmail.com>
2022-06-01 18:44:59 +0530

    Static Shopping Site

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\CartController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +16,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/',[PagesController::class,'index'])->name('home');
+
+Route::get('/shop',[ProductController::class,'index'])->name('shop');
+Route::get('/shop/{id}',[ProductController::class,'show'])->name('product');
+
+Route::get('/cart',[CartController::class,'cart'])->name('cart');
