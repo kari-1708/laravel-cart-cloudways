09a64d0f2fd6c04d2bbeca327a86bcf5a52b6c0b
kari-1708 <kmstudies143@gmail.com>
2022-06-01 22:52:32 +0530

    Updated Code for Shopping Cart

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,6 @@ Route::get('/shop',[ProductController::class,'index'])->name('shop');
 Route::get('/shop/{id}',[ProductController::class,'show'])->name('product');
 
 Route::get('/cart',[CartController::class,'cart'])->name('cart');
+Route::get('/add-to-cart/{id}',[CartController::class,'addToCart'])->name('add.to.cart');
+Route::post('/update-from-cart/{id}',[CartController::class,'update'])->name('update.from.cart');
+Route::get('/delete-from-cart/{id}',[CartController::class,'delete'])->name('delete.from.cart');
